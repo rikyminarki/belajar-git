e5e8f79c75f9e4d5efe9664766c9df47410417d4
riky <rikyminarki@gmail.com>
2020-10-01 14:38:58 +0700

    ditambahkan isi

--- index.php
+++ index.php
@@ -57,14 +57,14 @@
     <div class="container">
 
       <div class="logo float-left">
-        <h1 class="text-light"><a href="index.html"><span>Puskesmas Kampus</span></a></h1>
+        <h1 class="text-light"><a href="index.html"><span>Puskesmas Kampus Palembang</span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
 
       <nav class="nav-menu float-right d-none d-lg-block">
         <ul>
-          <li class="active"><a href="#header">Beranda</a></li>
+          <li class="active"><a href="#header">Home</a></li>
           <li><a href="#about">Tentang Kami</a></li>
           <li><a href="#services">Pelayanan</a></li>
           <li><a href="#portfolio">Griya Sehat</a></li>
